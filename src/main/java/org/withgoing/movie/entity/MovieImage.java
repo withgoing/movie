1f7c713d0bc8693d7ee6b347ef78d109b2de955e
lhlee <lhlee@lhleeui-MacBookAir.local>
2022-02-16 13:18:06 +0900

    Initial commit

---
+++ src/main/java/org/withgoing/movie/entity/MovieImage.java
@@ -0,0 +1,26 @@
+package org.withgoing.movie.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "movie")
+public class MovieImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+}
