8e17525efa4ebfeadd5fc9c45c56080845d726bc
withgoing <yhjh1211@gmail.com>
2022-02-22 20:40:40 +0900

    Feat: movie 서비스 기능 추가
    register.html 화면 개발 완료

--- src/main/java/org/withgoing/movie/service/MovieService.java
+++ src/main/java/org/withgoing/movie/service/MovieService.java
@@ -1,8 +1,44 @@
 package org.withgoing.movie.service;
 
 import org.withgoing.movie.dto.MovieDTO;
+import org.withgoing.movie.dto.MovieImageDTO;
+import org.withgoing.movie.entity.Movie;
+import org.withgoing.movie.entity.MovieImage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface MovieService {
 
     Long register(MovieDTO movieDTO);
+
+    default Map<String, Object> dtoToEntity(MovieDTO movieDTO) {
+        Map<String, Object> entityMap = new HashMap<>();
+
+        Movie movie = Movie.builder()
+                .mno(movieDTO.getMno())
+                .title(movieDTO.getTitle())
+                .build();
+
+        entityMap.put("movie", movie);
+
+        List<MovieImageDTO> imageDTOList = movieDTO.getImageDTOList();
+
+        if(imageDTOList != null && imageDTOList.size() > 0) {
+            List<MovieImage> movieImageList = imageDTOList.stream().map(movieImageDTO -> {
+               MovieImage movieImage = MovieImage.builder()
+                       .path(movieImageDTO.getPath())
+                       .imgName(movieImageDTO.getImgName())
+                       .uuid(movieImageDTO.getUuid())
+                       .movie(movie)
+                       .build();
+
+               return movieImage;
+            }).collect(Collectors.toList());
+        }
+
+        return entityMap;
+    }
 }
--- src/main/java/org/withgoing/movie/service/MovieServiceImpl.java
+++ src/main/java/org/withgoing/movie/service/MovieServiceImpl.java
@@ -3,8 +3,15 @@ package org.withgoing.movie.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.withgoing.movie.dto.MovieDTO;
+import org.withgoing.movie.entity.Movie;
+import org.withgoing.movie.entity.MovieImage;
+import org.withgoing.movie.repository.MovieImageRepository;
 import org.withgoing.movie.repository.MovieRepository;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Log4j2
@@ -12,10 +19,21 @@ import org.withgoing.movie.repository.MovieRepository;
 public class MovieServiceImpl implements MovieService {
 
     private final MovieRepository movieRepository;
+    private final MovieImageRepository movieImageRepository;
 
+    @Transactional
     @Override
     public Long register(MovieDTO movieDTO) {
-        return null;
+
+        Map<String, Object> entityMap = dtoToEntity(movieDTO);
+        Movie movie = (Movie) entityMap.get("movie");
+        List<MovieImage> movieImageList = (List<MovieImage>) entityMap.get("imgList");
+        movieRepository.save(movie);
+        movieImageList.forEach(movieImage -> {
+            movieImageRepository.save(movieImage);
+        });
+
+        return movie.getMno();
     }
 
 }
